f36713a75e35b6043b02c7555474b34b117359b6
LeaCReuben <leacruben@outlook.com>
2024-12-05 23:09:46 +0000

    Delete hbm.js

--- hbm.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById('menuBtn');
-const nav = document.getElementById('nav');
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open');
-    nav.classList.toggle('open');
-});
